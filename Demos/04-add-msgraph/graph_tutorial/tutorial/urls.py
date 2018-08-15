7e932fc1b77b7e09457db959cd357553bcbdfa7c
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-15 14:55:17 -0400

    Copied finished module 3 project to module 4

---
+++ Demos/04-add-msgraph/graph_tutorial/tutorial/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  # /tutorial
+  path('', views.home, name='home'),
+  path('signin', views.sign_in, name='signin'),
+  path('callback', views.callback, name='callback'),
+  path('signout', views.sign_out, name='signout'),
+]
