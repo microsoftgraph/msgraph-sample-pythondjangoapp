571ee2142b28128009188f09c6bff0d5e57b8164
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-15 14:54:16 -0400

    Added module 3

---
+++ Demos/03-add-aad-auth/graph_tutorial/tutorial/graph_helper.py
@@ -0,0 +1,10 @@
+from requests_oauthlib import OAuth2Session
+
+graph_url = 'https://graph.microsoft.com/v1.0'
+
+def get_user(token):
+  graph_client = OAuth2Session(token=token)
+  # Send GET to /me
+  user = graph_client.get('{0}/me'.format(graph_url))
+  # Return the JSON result
+  return user.json()
--- Demos/03-add-aad-auth/graph_tutorial/tutorial/urls.py
+++ Demos/03-add-aad-auth/graph_tutorial/tutorial/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
   # /tutorial
   path('', views.home, name='home'),
+  path('signin', views.sign_in, name='signin'),
+  path('callback', views.callback, name='callback'),
+  path('signout', views.sign_out, name='signout'),
 ]
